a4c18453c81166819a7c25b2d7be692197894d14
Nick Guenther <nick.guenther@polymtl.ca>
2021-11-11 01:46:37 -0500

    Build with Sphinx.

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+To build the docs:
+
+    pip install .[sphinx]
+    make html
+
+They will end up in _build/html/
+
+(this note is here and not in README.md because, in this case, README.md is the front-page)
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="neuropoly-internal-docs",
+    python_requires=">=3.6",
+    author="NeuroPoly",
+    extras_require={
+        "sphinx": [
+            "myst-parser",
+            "sphinx-book-theme",
+            # pinned because of this bug https://github.com/pydata/pydata-sphinx-theme/pull/509
+            # and that the patched sphinx-book-theme isn't out yet: https://github.com/executablebooks/sphinx-book-theme/issues/428#issuecomment-966021270
+            "sphinx~=4.2.0", # TODO: unpin when the next sphinx-book-theme is released
+        ],
+    },
+)
